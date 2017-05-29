bcc861fd416097083890b389380d06dd4100b82f
Vytautas Pranskunas <vytautas.pranskunas@gmail.com>
2017-05-29 22:31:20 +0300

    Updated to 1.4.0 version

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { ComponentState, HasStateActions, ReactComponentWithStateActions } from "react-state-rxjs";
 import { TodoStateActions } from "./actions/todo.actions";
-import { Test } from './test'
 
 @ComponentState(TodoStateActions)
 export class TodoDescription extends ReactComponentWithStateActions<any, any, TodoStateActions> {
