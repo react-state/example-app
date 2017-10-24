2ad3a8200d4db76fdb8eb65b6887b27f0d4f488e
KALLY\vytautas.pranskunas <vytautas.pranskunas@kallysoft.ch>
2017-10-24 16:01:41 +0200

    Upgraded to react-state-rxjs v2.0.0

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { Main } from "./components/main";
 import { ReactState } from "react-state-rxjs";
 import { initialState } from './initial-state';
 
+const isProd = false;
+
 ReactState.init((routerHistory: History) => {
     ReactDOM.render(<Main history={routerHistory} />, document.getElementById("example"))
-}, initialState)
+}, initialState, isProd)
