a9f4910045f8ba16c9f9993ceceaa45646fc3ba4
KALLY\vytautas.pranskunas <vytautas.pranskunas@kallysoft.ch>
2019-04-08 11:36:30 +0200

    Upgradet package version

--- src/components/description.tsx
+++ src/components/description.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { ComponentState, HasStateActions, ReactComponentWithStateActions } from "react-state-rxjs";
+import { ComponentState, ReactComponentWithStateActions } from "react-state-rxjs";
 import { TodoStateActions } from "./actions/todo.actions";
 
 @ComponentState(TodoStateActions)
 export class TodoDescription extends ReactComponentWithStateActions<any, any, TodoStateActions> {
-    render(){
-        return <div>{ this.actions.todoDescriptionAsync }</div>
+    render() {
+        return <div>{this.actions.todoDescriptionAsync}</div>
     }
 }
